c3863dda6fc751233e11467a27ae247e02aeede7
FabioStringaro <fastrin@gmail.com>
2018-01-09 11:14:09 +0100

    add assets

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import ( "flag"
+		 "log"
+		 "net/http"
+	 )
+
+
---
+++ model/model.go
@@ -0,0 +1,10 @@
+// The purpose of model is to separate how data is stored in the database from the ui //
+// as well as to contain any business logic an app might have. //
+
+package model
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
